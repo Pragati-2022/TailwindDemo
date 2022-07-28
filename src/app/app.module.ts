394f36df3053aedf2f0c0ad724665e910b052914
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-28 16:19:57 +0530

    removed header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     DashboardComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
