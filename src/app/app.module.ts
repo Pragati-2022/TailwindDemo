5b3fb7424e84462bdf5644d08f4003c62f6737df
Pragati Sanepara <pragati@atharvasystem.com>
2022-07-29 12:52:46 +0530

    import { BarChartComponent } from './pages/dashboard/second-chart-part/bar-chart/bar-chart.component'; added FirstPartComponent,  SecondChartPartComponent, LineChartComponent,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
+import { FirstPartComponent } from './pages/dashboard/first-part/first-part.component';
+import { SecondChartPartComponent } from './pages/dashboard/second-chart-part/second-chart-part.component';
+import { LineChartComponent } from './pages/dashboard/second-chart-part/line-chart/line-chart.component';
+import { BarChartComponent } from './pages/dashboard/second-chart-part/bar-chart/bar-chart.component';
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
     DashboardComponent,
+    FirstPartComponent,
+    SecondChartPartComponent,
+    LineChartComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
