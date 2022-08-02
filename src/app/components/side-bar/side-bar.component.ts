7e536241c2a0c21a86120f68caf53b1b5d4b7d73
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-02 17:58:21 +0530

    made dynamic titles of sidebar

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -4,16 +4,72 @@ import { CommonService } from 'src/app/services/common/common.service';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.scss']
+  styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent implements OnInit {
-  
-  constructor(public commonService : CommonService) { }
+  menu: any = [
+    {
+      routerLink: 'home',
+      title: 'TITLE.DASHBOARD',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.ICONS',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: 'maps',
+      title: 'TITLE.MAPS',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.USER_PROFILE',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: 'contactUs',
+      title: 'TITLE.CONTACT_US',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.LOGIN',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.PAGE_VIEW',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.DOCUMENTATION',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.GETTING_STARTED',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.FOUNDATION',
+      src: 'assets/icons/bricks.svg',
+    },
+    {
+      routerLink: '',
+      title: 'TITLE.COMPONENTS',
+      src: 'assets/icons/bricks.svg',
+    },
+  ];
+
+  constructor(public commonService: CommonService) {}
 
   ngOnInit(): void {
-    if(window.innerWidth < 450){
+    if (window.innerWidth < 450) {
       this.commonService.isOpen = false;
     }
   }
-
 }
