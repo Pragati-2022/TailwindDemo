9a8e8995ca9f899d6582162b5c42f469e934736b
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-02 17:17:39 +0530

    took barChart config from constant

--- src/app/pages/dashboard/second-chart-part/bar-chart/bar-chart.component.ts
+++ src/app/pages/dashboard/second-chart-part/bar-chart/bar-chart.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { ConstantClass } from 'src/app/constants/constants';
 
 @Component({
   selector: 'app-bar-chart',
@@ -15,8 +16,11 @@ import Chart from 'chart.js/auto';
 export class BarChartComponent implements OnInit, AfterViewInit {
   @ViewChild('barCanvas') private barCanvas!: ElementRef;
   barChart: any;
+  public constant;
 
-  constructor() {}
+  constructor() {
+    this.constant = ConstantClass;
+  }
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
@@ -24,49 +28,6 @@ export class BarChartComponent implements OnInit, AfterViewInit {
   }
 
   barChartMethod() {
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [100, 86, 55, 38, 15, 20],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-            barPercentage: 9,
-            barThickness: 5,
-            maxBarThickness: 8,
-            minBarLength: 50,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            max: 100,
-            ticks: {
-                stepSize: 5
-            }
-        }
-        },
-      },
-    });
+    this.barChart = new Chart(this.barCanvas.nativeElement, this.constant.barChartConfig);
   }
 }
