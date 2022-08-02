37d865a72932ad48bd3be4ed3281f6519ffc4781
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-02 17:02:52 +0530

    took columns and source from json file and added bold and bar view in two column

--- src/app/pages/dashboard/second-chart-part/social-traffic-table/social-traffic-table.component.ts
+++ src/app/pages/dashboard/second-chart-part/social-traffic-table/social-traffic-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GuiCellView, GuiColumn } from '@generic-ui/ngx-grid';
+import { Dashboard } from 'src/app/interfaces/dashboard';
+import { CallApiService } from 'src/app/services/callApi/call-api.service';
 
 @Component({
   selector: 'app-social-traffic-table',
@@ -7,50 +9,32 @@ import { GuiCellView, GuiColumn } from '@generic-ui/ngx-grid';
   styleUrls: ['./social-traffic-table.component.scss'],
 })
 export class SocialTrafficTableComponent implements OnInit {
-  constructor() {}
+  constructor(private callApiService: CallApiService) {}
 
-  ngOnInit(): void {}
-  columns: Array<GuiColumn> = [
-    {
-      header: 'Name',
-      field: 'name',
-    },
-    {
-      header: 'Job',
-      field: 'job',
-    },
-    {
-      header: '',
-      field: 'age',
-      view: GuiCellView.BAR
-    },
-  ];
+  ngOnInit(): void {
+    this.getColumns();
+  }
+  getColumns() {
+    this.callApiService
+      .getAll('assets/fakeData/dashboard.json')
+      .subscribe((data: Dashboard) => {
+        this.columns = data.social_traffic.columns;
+        this.columns.map((data : any) => {
+         const setBold:any = ['job'];
+         const setBAR:any = ['age','progress'];
 
-  source: Array<any> = [
-    {
-      name: 'Brad',
-      job: 'programmer',
-      age: '40',
-    },
-    {
-      name: 'John',
-      job: 'athlete',
-      age: '100',
-    },
-    {
-      name: 'John',
-      job: 'athlete',
-      age: '79',
-    },
-    {
-      name: 'John',
-      job: 'athlete',
-      age: '10',
-    },
-    {
-      name: 'Eve',
-      job: 'artist',
-      age: '50',
-    },
-  ];
+         if(setBold.includes(data.field.toLowerCase())){
+           data.view = GuiCellView.BOLD;
+         }
+         if(setBAR.includes(data.field.toLowerCase())){
+           data.view = GuiCellView.BAR;
+         }
+        });
+
+        this.source = data.social_traffic.source;
+      });
+  }
+
+  columns: Array<GuiColumn> = [];
+  source: Array<any> = [];
 }
