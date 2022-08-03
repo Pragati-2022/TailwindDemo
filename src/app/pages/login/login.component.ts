245217cc71a7747aae3f63357e1ad169eeacdaa8
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-03 19:18:17 +0530

    login page with reactive form and responsive

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ConstantClass } from 'src/app/constants/constants';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { CommonService } from 'src/app/services/common/common.service';
+import { Regex } from 'src/app/utils/regex';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public constant;
 
-  constructor() { }
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'MESSAGE.REQUIRED' },
+      { type: 'pattern', message: 'MESSAGE.EMAIL_PATTERN' },
+    ],
+    password: [
+      { type: 'required', message: 'MESSAGE.REQUIRED' },
+      { type: 'pattern', message: 'MESSAGE.EMAIL_PATTERN' },
+    ]
+  };
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private router : Router, public commonService : CommonService, public authService : AuthService) {
+    this.constant = ConstantClass;
+    this.initializeForm();
   }
 
+  ngOnInit(): void { }
+
+  initializeForm() {
+    this.constant.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(Regex.emailPattern)]],
+      password: ['', [Validators.required, Validators.pattern(Regex.passwordPattern)]]
+    });
+  }
+
+  get _loginForm() {
+    return this.constant.loginForm.controls;
+  }
+
+  onSubmit(){
+    localStorage.setItem('loggedIn', String(true));
+    this.router.navigate(['home']);
+    this.authService.onlogin();
+    console.log(this.constant.loginForm.value);
+  }
 }
