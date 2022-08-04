3951850984a1dfba646c848a849c2f0d0a6d3a35
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-04 12:59:06 +0530

    checked user is loggedIn and current page is not pageNotFound for sideBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from './services/auth/auth.service';
+import { CommonService } from './services/common/common.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'TailwindDemo';
 
-  constructor(private translate: TranslateService, public authService : AuthService) {
+  constructor(private translate: TranslateService, public commonService : CommonService) {
     translate.setDefaultLang('en');
-    translate.use('en');
+    translate.use('en');    
   }
 }
