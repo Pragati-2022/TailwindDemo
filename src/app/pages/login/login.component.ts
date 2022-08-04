09897ad9604853c28535a9927c7c9d05109bcabf
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-04 13:00:01 +0530

    user LoggedIn then localStorage setItem

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ConstantClass } from 'src/app/constants/constants';
-import { AuthService } from 'src/app/services/auth/auth.service';
 import { CommonService } from 'src/app/services/common/common.service';
 import { Regex } from 'src/app/utils/regex';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   public constant;
@@ -22,20 +21,30 @@ export class LoginComponent implements OnInit {
     password: [
       { type: 'required', message: 'MESSAGE.REQUIRED' },
       { type: 'pattern', message: 'MESSAGE.EMAIL_PATTERN' },
-    ]
+    ],
   };
 
-  constructor(private formBuilder: FormBuilder, private router : Router, public commonService : CommonService, public authService : AuthService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    public commonService: CommonService,
+  ) {
     this.constant = ConstantClass;
     this.initializeForm();
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   initializeForm() {
     this.constant.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.pattern(Regex.emailPattern)]],
-      password: ['', [Validators.required, Validators.pattern(Regex.passwordPattern)]]
+      email: [
+        '',
+        [Validators.required, Validators.pattern(Regex.emailPattern)],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(Regex.passwordPattern)],
+      ],
     });
   }
 
@@ -43,10 +52,10 @@ export class LoginComponent implements OnInit {
     return this.constant.loginForm.controls;
   }
 
-  onSubmit(){
-    localStorage.setItem('loggedIn', String(true));
+  onSubmit() {
+    localStorage.setItem('loggedIn', btoa(this.constant.loginForm.value.email));
     this.router.navigate(['home']);
-    this.authService.onlogin();
+    this.commonService.isLoggedIn = atob(localStorage.getItem('loggedIn') || '');
     console.log(this.constant.loginForm.value);
   }
 }
